f14096582894371aaa0b09d259b9a849de78c779
EiseZ <65886482+EiseZ@users.noreply.github.com>
2021-04-20 11:27:58 +0200

    Added personal weather info

--- js/weather.js
+++ js/weather.js
@@ -13,7 +13,7 @@ var tempUnit = 'C';
 
 const KELVIN = 273.15;
 // Use your own key for the Weather, Get it here: https://openweathermap.org/
-const key = 'aa5b0a76dfbf87441928fb3cc32d3d72';
+const key = '7a25e277a259afb736bda63ee48af751';
 
 // Set Position function
 setPosition();
@@ -21,8 +21,8 @@ setPosition();
 function setPosition(position) {
   // Here you can change your position
   // You can use https://www.latlong.net/ to get it! (I use San Francisco as an example)
-  let latitude = 37.774929;
-  let longitude = -122.419418;
+  let latitude = 52.075031;
+  let longitude = 4.359280;
 
   getWeather(latitude, longitude);
 }
